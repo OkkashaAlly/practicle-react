d48d488f067240323ae57e4da6d343c228c22947
OkkashaAlly <steamnature006@gmail.com>
2022-10-29 21:14:26 +0300

    PAGES: launchpad/dutch-auction - page added && COMPONENTS: DutchAuctionForm - component added

---
+++ components/DutchAuctionForm.jsx
@@ -0,0 +1,112 @@
+import { Formik, Form } from "formik";
+import * as Yup from "yup";
+import FormikControl from "./FormikControl";
+import InputNote from "./shared/InputNote";
+
+// ================================
+// FORMIK CONTAINER COMPONENT =====
+// ================================
+const FairlaunchForm = () => {
+  const feeOptions = [
+    {
+      key: "5%",
+      value: "5%",
+    },
+    {
+      key: "2%",
+      value: "2%",
+    },
+  ];
+
+  const currencyOptions = [
+    {
+      key: "BNB",
+      value: "bnb",
+    },
+    {
+      key: "BUSD",
+      value: "busd",
+    },
+    {
+      key: "USDT",
+      value: "usdt",
+    },
+    {
+      key: "USDC",
+      value: "usdc",
+    },
+  ];
+
+  const initialValues = {
+    tokenAddress: "",
+    tokenCurrency: "bnb",
+    feeOptions: "5%",
+  };
+  const validationSchema = Yup.object({
+    tokenAddress: Yup.string().required("Required"),
+    tokenCurrency: Yup.string().required("Required"),
+    feeOptions: Yup.string().required("Required"),
+  });
+
+  const handleSubmit = (values, submitProps) => {
+    console.log("Form values: ", values);
+    submitProps.setSubmitting(false);
+    submitProps.resetForm();
+  };
+
+  /////////////////
+  // RETURN =======
+  return (
+    <Formik
+      initialValues={initialValues}
+      validationSchema={validationSchema}
+      onSubmit={handleSubmit}
+    >
+      {formik => (
+        <Form className="p-4 bg-neutral-800 rounded-md flex flex-col gap-4">
+          {/* Input  */}
+          <FormikControl
+            control={"input"}
+            label={"Token Address"}
+            type={"text"}
+            name={"tokenAddress"}
+            placeholder={"Ex: 0x000..."}
+          />
+
+          <InputNote note={"Pool creation fee: 1 BNB"} />
+
+          {/* Radio  */}
+          <FormikControl
+            control={"radio"}
+            label={"Currency"}
+            name={"tokenCurrency"}
+            options={currencyOptions}
+          />
+
+          <InputNote
+            note={`Users will pay with ${formik.values.tokenCurrency} for your token`}
+          />
+
+          <FormikControl
+            control={"radio"}
+            label={"Fee Options"}
+            name={"feeOptions"}
+            options={feeOptions}
+            dynamicRadio
+            dynamicText={" raised only"}
+          />
+
+          <button
+            className="w-40 font-bold py-3 px-4 bg-[#b13564] hover:bg-[#89244a] text-neutral-300 rounded-full capitalize text-lg"
+            type="submit"
+          >
+            Submit
+          </button>
+        </Form>
+      )}
+    </Formik>
+  );
+};
+
+// EXPORT ====================
+export default FairlaunchForm;
---
+++ pages/launchpad/dutch-auction.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import DutchAuctionForm from "../../components/DutchAuctionForm";
+
+const StepItem = ({ title, subtitle, number }) => (
+  <div className="flex w-full">
+    <div className="flex gap-2">
+      <p className="h-6 w-6 flex items-center justify-center  bg-rose-400 text-slate-800 rounded-full">
+        {number}
+      </p>
+      <div className="">
+        <div className="flex-1 flex gap-3 items-center">
+          <h6>{title}</h6>{" "}
+          <span className="h-[0.5px]  block bg-neutral-300 flex-1" />
+        </div>
+        <p className="w-[80%] text-neutral-500">{subtitle}</p>
+      </div>
+    </div>
+  </div>
+);
+
+const dutchAuction = () => {
+  return (
+    <div className="p-6">
+      {/* steps  */}
+      {/* <div className="flex mt-3 gap-4">
+        <StepItem
+          number={1}
+          title={"Verify Token"}
+          subtitle={"Enter the token address and verify"}
+        />
+        <StepItem
+          number={1}
+          title={"Subscription"}
+          subtitle={
+            "Enter the subscription pool information that you want to raise, that should be enter all details about your presale"
+          }
+        />
+        <StepItem
+          number={3}
+          title={"Add Additional Info"}
+          subtitle={"Let people know who you are"}
+        />
+        <StepItem
+          number={4}
+          title={"Finish"}
+          subtitle={"Review your information"}
+        />
+      </div> */}
+
+      {/* form  */}
+      <DutchAuctionForm />
+    </div>
+  );
+};
+
+export default dutchAuction;
