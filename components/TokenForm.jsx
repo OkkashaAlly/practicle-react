ba40276826b073e38080eadf76c479601edcaa78
OkkashaAlly <steamnature006@gmail.com>
2022-10-30 00:21:50 +0300

    COMPONENTS: TokenForm - BabyControls Extended component added

--- components/TokenForm.jsx
+++ components/TokenForm.jsx
@@ -31,6 +31,33 @@ const TokenForm = () => {
 
   const [tokenTypeSelected, setTokenTypeSelected] = useState("standard");
 
+  const routerOptions = [
+    {
+      key: "Select Router Exchange",
+      value: "",
+    },
+    {
+      key: "Pancakeswap",
+      value: "0x10ED43C718714eb63d5aA57B78B54704E256024E",
+    },
+    {
+      key: "ApeSwap",
+      value: "0xcF0feBd3f17CEf5b47b0cD257aCf6025c5BFf3b7",
+    },
+    {
+      key: "MDex",
+      value: "0xf400087A4c94c52C6540A325CB702DE3ee7CB37f",
+    },
+    {
+      key: "BiSwap",
+      value: "0x3a6d8cA21D1CF76F653A67577FA0D27453350dD8",
+    },
+    {
+      key: "KnightSwap",
+      value: "0x05E61E0cDcD2170a76F9568a110CEe3AFdD6c46f",
+    },
+  ];
+
   const handleSubmit = (values, submitProps) => {
     console.log("Form values: ", values);
     submitProps.setSubmitting(false);
@@ -39,14 +66,6 @@ const TokenForm = () => {
 
   /////////////////
   // RETURN =======
-  // switch ("bb") {
-  //   case "liquidity":
-  //     return <LiquidityControls handleSubmit={handleSubmit} />;
-
-  //   default:
-  //     return <StandardControls handleSubmit={handleSubmit} />;
-  // }
-
   return (
     <div className="p-4 bg-neutral-800 rounded-md ">
       {/* selector  */}
@@ -72,7 +91,11 @@ const TokenForm = () => {
       <InputNote note={"0.2 BNB"} />
 
       {/* Form  */}
-      <Forms type={tokenTypeSelected} handleSubmit={handleSubmit} />
+      <Forms
+        type={tokenTypeSelected}
+        routerOptions={routerOptions}
+        handleSubmit={handleSubmit}
+      />
     </div>
   );
 };
@@ -80,10 +103,23 @@ const TokenForm = () => {
 //////////////////////
 // EXTENDED COMPONENTS
 
-const Forms = ({ type, handleSubmit }) => {
+const Forms = ({ type, handleSubmit, routerOptions }) => {
   switch (type) {
     case "liquidity":
-      return <LiquidityControls handleSubmit={handleSubmit} />;
+      return (
+        <LiquidityControls
+          routerOptions={routerOptions}
+          handleSubmit={handleSubmit}
+        />
+      );
+
+    case "baby":
+      return (
+        <BabyControls
+          routerOptions={routerOptions}
+          handleSubmit={handleSubmit}
+        />
+      );
 
     default:
       return <StandardControls handleSubmit={handleSubmit} />;
@@ -138,34 +174,7 @@ const StandardControls = ({ handleSubmit }) => {
   );
 };
 
-const LiquidityControls = ({ handleSubmit }) => {
-  const routerOptions = [
-    {
-      key: "Select Router Exchange",
-      value: "",
-    },
-    {
-      key: "Pancakeswap",
-      value: "0x10ED43C718714eb63d5aA57B78B54704E256024E",
-    },
-    {
-      key: "ApeSwap",
-      value: "0xcF0feBd3f17CEf5b47b0cD257aCf6025c5BFf3b7",
-    },
-    {
-      key: "MDex",
-      value: "0xf400087A4c94c52C6540A325CB702DE3ee7CB37f",
-    },
-    {
-      key: "BiSwap",
-      value: "0x3a6d8cA21D1CF76F653A67577FA0D27453350dD8",
-    },
-    {
-      key: "KnightSwap",
-      value: "0x05E61E0cDcD2170a76F9568a110CEe3AFdD6c46f",
-    },
-  ];
-
+const LiquidityControls = ({ handleSubmit, routerOptions }) => {
   const initialValues = {
     tokenName: "",
     tokenSymbol: "",
@@ -224,7 +233,7 @@ const LiquidityControls = ({ handleSubmit }) => {
               placeholder={"Ex: 1"}
             />
           </div>
-          
+
           <div className="flex gap-3">
             {/* Input  */}
             <FormikControl
@@ -241,6 +250,121 @@ const LiquidityControls = ({ handleSubmit }) => {
               type={"text"}
               name={"charityPercent"}
               placeholder={"0 - 25"}
+            />
+          </div>
+
+          <button
+            className="w-40 font-bold py-3 px-4 bg-[#b13564] hover:bg-[#89244a] text-neutral-300 rounded-full capitalize text-lg"
+            type="submit"
+          >
+            create
+          </button>
+        </Form>
+      )}
+    </Formik>
+  );
+};
+const BabyControls = ({ handleSubmit, routerOptions }) => {
+  const initialValues = {
+    tokenName: "",
+    tokenSymbol: "",
+    tokenTotalSupply: "",
+    router: "",
+    rewardToken: "",
+    minTokenBalance: "",
+    tokenRewardFee: "",
+    autoLiquidity: "",
+    marketingFee: "",
+    marketingWallet: "",
+  };
+
+  const validationSchema = Yup.object({
+    tokenName: Yup.string().required("Token name cannot be empty"),
+    tokenSymbol: Yup.string().required("Required"),
+    tokenTotalSupply: Yup.string().required("Required"),
+    router: Yup.string().required("Required"),
+    rewardToken: Yup.string().required("Required"),
+    minTokenBalance: Yup.string().required("Required"),
+    tokenRewardFee: Yup.string().required("Required"),
+    autoLiquidity: Yup.string().required("Required"),
+    marketingFee: Yup.string().required("Required"),
+    marketingWallet: Yup.string().required("Required"),
+  });
+
+  /////////////////
+  // RETURN =======
+  return (
+    <Formik
+      initialValues={initialValues}
+      validationSchema={validationSchema}
+      onSubmit={handleSubmit}
+    >
+      {formik => (
+        <Form className="mt-4 flex flex-col gap-4">
+          <CommonControls />
+
+          {/* Select  */}
+          <FormikControl
+            control={"select"}
+            label={"Router"}
+            name={"router"}
+            options={routerOptions}
+          />
+
+          <div className="flex gap-3">
+            {/* Input  */}
+            <FormikControl
+              control={"input"}
+              label={"Reward token"}
+              type={"text"}
+              name={"rewardToken"}
+              placeholder={"Ex: 1"}
+            />
+            {/* Input  */}
+            <FormikControl
+              control={"input"}
+              label={"Minimum token balance for dividends"}
+              type={"number"}
+              name={"minTokenBalance"}
+              placeholder={"Ex: 100,000"}
+            />
+          </div>
+
+          <div className="flex gap-3">
+            {/* Input  */}
+            <FormikControl
+              control={"input"}
+              label={"Token reward fee (%)"}
+              type={"number"}
+              name={"tokenRewardFee"}
+              placeholder={"0 - 100"}
+            />
+            {/* Input  */}
+            <FormikControl
+              control={"input"}
+              label={"Auto add liquidity (%)"}
+              type={"number"}
+              name={"autoLiquidity"}
+              placeholder={"0 - 100"}
+            />
+          </div>
+
+          <div className="flex gap-3">
+            {/* Input  */}
+            <FormikControl
+              control={"input"}
+              label={"Marketing fee (%)"}
+              type={"number"}
+              name={"marketingFee"}
+              placeholder={"0 - 100"}
+            />
+            {/* Input  */}
+            <FormikControl
+              control={"input"}
+              label={"Marketing wallet"}
+              type={"text"}
+              name={"marketingWallet"}
+              placeholder={"0 - 100"}
             />
           </div>
 
