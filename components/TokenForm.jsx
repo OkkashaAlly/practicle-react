aba572596084081bafcb6e7bc351c4a639dd355e
OkkashaAlly <steamnature006@gmail.com>
2022-10-29 23:40:29 +0300

    COMPONENTS: TokenForm - StarndardControls Extended componet added

--- components/TokenForm.jsx
+++ components/TokenForm.jsx
@@ -1,5 +1,8 @@
+import { useState } from "react";
+
 import { Formik, Form } from "formik";
 import * as Yup from "yup";
+
 import FormikControl from "./FormikControl";
 import InputNote from "./shared/InputNote";
 
@@ -7,52 +10,100 @@ import InputNote from "./shared/InputNote";
 // FORMIK CONTAINER COMPONENT =====
 // ================================
 const TokenForm = () => {
-  const feeOptions = [
+  const tokenTypeOptions = [
     {
-      key: "5%",
-      value: "5%",
+      key: "Standard Token",
+      value: "standard",
     },
     {
-      key: "2%",
-      value: "2%",
-    },
-  ];
-
-  const currencyOptions = [
-    {
-      key: "BNB",
-      value: "bnb",
+      key: "Liquidity Generator Token",
+      value: "liquidity",
     },
     {
-      key: "BUSD",
-      value: "busd",
+      key: "Baby Token",
+      value: "baby",
     },
     {
-      key: "USDT",
-      value: "usdt",
-    },
-    {
-      key: "USDC",
-      value: "usdc",
+      key: "Buyback Baby Token",
+      value: "buyback",
     },
   ];
 
-  const initialValues = {
-    tokenAddress: "",
-    tokenCurrency: "bnb",
-    feeOptions: "5%",
-  };
-  const validationSchema = Yup.object({
-    tokenAddress: Yup.string().required("Required"),
-    tokenCurrency: Yup.string().required("Required"),
-    feeOptions: Yup.string().required("Required"),
-  });
+  const [tokenTypeSelected, setTokenTypeSelected] = useState("standard");
 
   const handleSubmit = (values, submitProps) => {
     console.log("Form values: ", values);
     submitProps.setSubmitting(false);
     submitProps.resetForm();
   };
+
+  /////////////////
+  // RETURN =======
+  // switch ("bb") {
+  //   case "liquidity":
+  //     return <LiquidityControls handleSubmit={handleSubmit} />;
+
+  //   default:
+  //     return <StandardControls handleSubmit={handleSubmit} />;
+  // }
+
+  return (
+    <div className="p-4 bg-neutral-800 rounded-md ">
+      {/* selector  */}
+      <div className="flex flex-col mb-4">
+        <label htmlFor="" className={"capitalize mb-1"}>
+          Token Type
+        </label>
+        <select
+          className="p-2 bg-neutral-700 border border-neutral-300 rounded focus:outline-rose-500"
+          name=""
+          onChange={e => {
+            setTokenTypeSelected(e.target.value);
+          }}
+        >
+          {tokenTypeOptions.map(opt => (
+            <option key={opt.value} value={opt.value}>
+              {opt.key}
+            </option>
+          ))}
+        </select>
+      </div>
+
+      <InputNote note={"0.2 BNB"} />
+
+      {/* Form  */}
+      <Forms type={tokenTypeSelected} handleSubmit={handleSubmit} />
+    </div>
+  );
+};
+
+//////////////////////
+// EXTENDED COMPONENTS
+
+const Forms = ({ type, handleSubmit }) => {
+  switch (type) {
+    case "liquidity":
+      return <LiquidityControls handleSubmit={handleSubmit} />;
+
+    default:
+      return <StandardControls handleSubmit={handleSubmit} />;
+  }
+};
+
+const StandardControls = ({ handleSubmit }) => {
+  const initialValues = {
+    tokenName: "",
+    tokenSymbol: "",
+    tokenDecimals: "",
+    tokenTotalSupply: "",
+  };
+
+  const validationSchema = Yup.object({
+    tokenName: Yup.string().required("Token name cannot be empty"),
+    tokenSymbol: Yup.string().required("Required"),
+    tokenTotalSupply: Yup.string().required("Required"),
+    tokenDecimals: Yup.string().required("Required"),
+  });
 
   /////////////////
   // RETURN =======
@@ -63,50 +114,132 @@ const TokenForm = () => {
       onSubmit={handleSubmit}
     >
       {formik => (
-        <Form className="p-4 bg-neutral-800 rounded-md flex flex-col gap-4">
+        <Form className="mt-4 flex flex-col gap-4">
+          <CommonControls />
+
           {/* Input  */}
           <FormikControl
             control={"input"}
-            label={"Token Address"}
-            type={"text"}
-            name={"tokenAddress"}
-            placeholder={"Ex: 0x000..."}
-          />
-
-          <InputNote note={"Pool creation fee: 1 BNB"} />
-
-          {/* Radio  */}
-          <FormikControl
-            control={"radio"}
-            label={"Currency"}
-            name={"tokenCurrency"}
-            options={currencyOptions}
-          />
-
-          <InputNote
-            note={`Users will pay with ${formik.values.tokenCurrency} for your token`}
-          />
-
-          <FormikControl
-            control={"radio"}
-            label={"Fee Options"}
-            name={"feeOptions"}
-            options={feeOptions}
-            dynamicRadio
-            dynamicText={" raised only"}
+            label={"Token Decimals"}
+            type={"number"}
+            name={"tokenDecimals"}
+            placeholder={"Ex: 18"}
           />
 
           <button
             className="w-40 font-bold py-3 px-4 bg-[#b13564] hover:bg-[#89244a] text-neutral-300 rounded-full capitalize text-lg"
             type="submit"
           >
-            Submit
+            create
           </button>
         </Form>
       )}
     </Formik>
   );
 };
+
+const LiquidityControls = ({ handleSubmit }) => {
+  const routerOptions = [
+    {
+      key: "Select Router Exchange",
+      value: "",
+    },
+    {
+      key: "Pancakeswap",
+      value: "0x10ED43C718714eb63d5aA57B78B54704E256024E",
+    },
+    {
+      key: "ApeSwap",
+      value: "0xcF0feBd3f17CEf5b47b0cD257aCf6025c5BFf3b7",
+    },
+    {
+      key: "MDex",
+      value: "0xf400087A4c94c52C6540A325CB702DE3ee7CB37f",
+    },
+    {
+      key: "BiSwap",
+      value: "0x3a6d8cA21D1CF76F653A67577FA0D27453350dD8",
+    },
+    {
+      key: "KnightSwap",
+      value: "0x05E61E0cDcD2170a76F9568a110CEe3AFdD6c46f",
+    },
+  ];
+
+  const initialValues = {
+    tokenName: "",
+    tokenSymbol: "",
+    tokenTotalSupply: "",
+    router: "",
+  };
+
+  const validationSchema = Yup.object({
+    tokenName: Yup.string().required("Token name cannot be empty"),
+    tokenSymbol: Yup.string().required("Required"),
+    tokenTotalSupply: Yup.string().required("Required"),
+    router: Yup.string().required("Required"),
+  });
+
+  /////////////////
+  // RETURN =======
+  return (
+    <Formik
+      initialValues={initialValues}
+      validationSchema={validationSchema}
+      onSubmit={handleSubmit}
+    >
+      {formik => (
+        <Form className="mt-4 flex flex-col gap-4">
+          <CommonControls />
+
+          {/* Select  */}
+          <FormikControl
+            control={"select"}
+            label={"Router"}
+            name={"router"}
+            options={routerOptions}
+          />
+
+          <button
+            className="w-40 font-bold py-3 px-4 bg-[#b13564] hover:bg-[#89244a] text-neutral-300 rounded-full capitalize text-lg"
+            type="submit"
+          >
+            create
+          </button>
+        </Form>
+      )}
+    </Formik>
+  );
+};
+
+const CommonControls = () => (
+  <>
+    {/* Input  */}
+    <FormikControl
+      control={"input"}
+      label={"Token Name"}
+      type={"text"}
+      name={"tokenName"}
+      placeholder={"Ex: Kripte"}
+    />
+    {/* Input  */}
+    <FormikControl
+      control={"input"}
+      label={"Token Symbol"}
+      type={"text"}
+      name={"tokenSymbol"}
+      placeholder={"Ex: KPT"}
+    />
+    {/* Input  */}
+    <FormikControl
+      control={"input"}
+      label={"Total supply"}
+      type={"number"}
+      name={"tokenTotalSupply"}
+      placeholder={"Ex: 100,000,000"}
+    />
+  </>
+);
 
 // EXPORT ====================
 export default TokenForm;
--- components/shared/Select.jsx
+++ components/shared/Select.jsx
@@ -4,10 +4,14 @@ import TextError from "./TextError";
 const Select = ({ label, name, options }) => {
   return (
     <div className="flex flex-col">
-      <label htmlFor={name} className={"capitalize"}>
+      <label htmlFor={name} className={"capitalize mb-1"}>
         {label}:
       </label>
-      <Field as="select" className="p-3 border border-black" name={name}>
+      <Field
+        as="select"
+        className="p-2 bg-neutral-700 border border-neutral-300 rounded focus:outline-rose-500"
+        name={name}
+      >
         {options.map(opt => (
           <option key={opt.value} value={opt.value}>
             {opt.key}
