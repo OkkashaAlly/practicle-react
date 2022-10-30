abf6b93bb0a93c43daf59cc47bb7f6b4dad45a0c
OkkashaAlly <steamnature006@gmail.com>
2022-10-30 14:21:11 +0300

    LAYOUTS: Sidebar - Dynamic active link

--- layout/SideBar.jsx
+++ layout/SideBar.jsx
@@ -1,3 +1,4 @@
+import { useRouter } from "next/router";
 import Link from "next/link";
 import { useState } from "react";
 
@@ -95,6 +96,7 @@ const SideBar = ({
   //   },
   // ];
 
+  // console.log(router);
   return (
     <>
       <aside
@@ -103,15 +105,22 @@ const SideBar = ({
         } border-r border-neutral-600 p-4 animation-300 hidden md:block`}
       >
         <nav className="">
-          {navLinks.map((link, i) => (
-            <NavItem
-              key={i}
-              item={link}
-              sidebarOpen={sidebarOpen}
-              setSidebarOpen={setSidebarOpen}
-              setMobileMenu={setMobileMenu}
-            />
-          ))}
+          {navLinks.map((link, i) => {
+            // console.log(link.dropdowns);
+
+            // link.active = router.pathname === link.pathname ? true : false;
+            // link.active = router.pathname === link.dropdowns.pathname ? true : false;
+
+            return (
+              <NavItem
+                key={i}
+                item={link}
+                sidebarOpen={sidebarOpen}
+                setSidebarOpen={setSidebarOpen}
+                setMobileMenu={setMobileMenu}
+              />
+            );
+          })}
         </nav>
       </aside>
 
@@ -128,34 +137,51 @@ const SideBar = ({
 
 ///////////////////////
 // EXTENDED COMPONENTS
-const NavItem = ({ item, sidebarOpen, setSidebarOpen, setMobileMenu }) => {
+const NavItem = ({ item, sidebarOpen, setMobileMenu }) => {
   const [dropdownOpen, setDropdownOpen] = useState(false);
 
+  const router = useRouter();
+
+  // item.dropdowns?.map((dropdown, i) => {
+  //   item.active = router.pathname === item.pathname ? true : false;
+  //   item.active = router.pathname === dropdown.pathname ? true : false;
+  // item.active = router.pathname === item.pathname ? true : false;
+  // });
+  // console.log(router.pathname, dropdown.pathname);
+
   return item.single ? (
-    <Link href={item.pathname}>
-      <div
-        onClick={_ => {
-          setMobileMenu(false);
-          setDropdownOpen(false);
-          // setSidebarOpen(false);
-        }}
-        className={`p-2 my-2 cursor-pointer ${
-          item.active && "bg-neutral-700"
-        } hover:bg-neutral-700 text-neutral-300 rounded-lg flex items-center gap-3`}
-      >
-        <span className="h-8 w-8 p-2 bg-rose-100 rounded-full flex items-center justify-center">
-          {item.icon()}
-        </span>
-        <span className={`${!sidebarOpen && "hidden"} capitalize`}>
-          {item.text}
-        </span>
-      </div>
-    </Link>
+    <>
+      {" "}
+      {(item.active = router.pathname === item.pathname ? true : false)}
+      <Link href={item.pathname}>
+        <div
+          onClick={_ => {
+            setMobileMenu(false);
+            setDropdownOpen(false);
+            // setSidebarOpen(false);
+          }}
+          className={`p-2 my-2 cursor-pointer ${
+            item.active && "bg-neutral-700"
+          } hover:bg-neutral-700 text-neutral-300 rounded-lg flex items-center gap-3`}
+        >
+          <span className="h-8 w-8 p-2 bg-rose-100 rounded-full flex items-center justify-center">
+            {item.icon()}
+          </span>
+          <span className={`${!sidebarOpen && "hidden"} capitalize`}>
+            {item.text}
+          </span>
+        </div>
+      </Link>
+    </>
   ) : (
     <>
+      {" "}
+      {item.dropdowns.map(
+        drop => (drop.active = router.pathname === drop.pathname ? true : false)
+      )}
       <div
         className={`p-2 my-2 cursor-pointer ${
-          item.active && "bg-neutral-700"
+          router.pathname.includes(item.text) && "bg-neutral-700"
         } hover:bg-neutral-700 text-neutral-300 rounded-lg `}
         onClick={_ => {
           setDropdownOpen(!dropdownOpen);
@@ -175,14 +201,18 @@ const NavItem = ({ item, sidebarOpen, setSidebarOpen, setMobileMenu }) => {
           <div
             className={`${
               !dropdownOpen && "hidden"
-            } absolute -top-6 left-14 bg-neutral-800 rounded w-52 mb-4`}
+            } absolute -top-6 left-14 bg-neutral-800 rounded duration-300 w-52 mb-4`}
           >
             {item.dropdowns.map((drop, i) => (
-              <Link href={drop.pathname}>
+              <Link href={drop.pathname} key={i}>
                 <div
-                  key={i}
+                  
                   onClick={_ => setDropdownOpen(false)}
-                  className="capitalize flex items-center py-2 text-neutral-400 hover:text-neutral-100 ml-4"
+                  className={`capitalize flex items-center py-2 ${
+                    drop.active
+                      ? "text-neutral-100 underline"
+                      : "text-neutral-400 "
+                  } hover:text-neutral-100 ml-4`}
                 >
                   <RiCheckboxBlankCircleFill className="mr-2 h-2 w-2" />
                   {drop.text}
@@ -197,15 +227,17 @@ const NavItem = ({ item, sidebarOpen, setSidebarOpen, setMobileMenu }) => {
             } rounded  bg-neutral-800 mb-4`}
           >
             {item.dropdowns.map((drop, i) => (
-              <Link href={drop.pathname}>
+              <Link href={drop.pathname} key={i}>
                 <div
-                  key={i}
+                  
                   onClick={_ => {
                     setMobileMenu(false);
-                    // setDropdownOpen(false);
-                    // setSidebarOpen(false);
                   }}
-                  className="capitalize flex items-center py-2 text-neutral-400 hover:text-neutral-100 ml-4"
+                  className={`capitalize flex items-center py-2 ${
+                    drop.active
+                      ? "text-neutral-100 underline"
+                      : "text-neutral-400 "
+                  } hover:text-neutral-100 ml-4`}
                 >
                   <RiCheckboxBlankCircleFill className="mr-2 h-2 w-2" />
                   {drop.text}
