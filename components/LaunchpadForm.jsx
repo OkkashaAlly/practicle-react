bdda5a22a431dc2848f709a1745372b6a89b33ca
OkkashaAlly <steamnature006@gmail.com>
2022-10-31 23:54:31 +0300

    PAGES: launchpad/create - step 2 intergrated

--- components/LaunchpadForm.jsx
+++ components/LaunchpadForm.jsx
@@ -1,4 +1,3 @@
-
 import { Formik, Form } from "formik";
 import * as Yup from "yup";
 
@@ -9,27 +8,45 @@ import InputNote from "./shared/InputNote";
 // ================================
 // FORMIK CONTAINER COMPONENT =====
 // ================================
-const LaunchpadForm = ({page, setPage}) => {
-  
-
+const LaunchpadForm = ({ page, setPage }) => {
   const initialValues = {
     tokenAddress: "",
     tokenCurrency: "bnb",
     feeOptions: "5%",
     listingOptions: "autoListing",
+    hardcapTokens: "",
+    softcapTokens: "",
+    hardcapTokensPerUser: "",
+    subscriptionRate: "",
+    listingRate: "",
+    whitelist: "disabled",
+    router: "disabled",
+    liquidityPercent: "",
+    refundType: "refund",
+    liquidityLockTime: "",
   };
+
   const validationSchema = Yup.object({
     tokenAddress: Yup.string().required("Required"),
     tokenCurrency: Yup.string().required("Required"),
     feeOptions: Yup.string().required("Required"),
+    hardcapTokens: Yup.string().required("Required"),
+    softcapTokens: Yup.string().required("Required"),
+    hardcapTokensPerUser: Yup.string().required("Required"),
+    subscriptionRate: Yup.string().required("Required"),
+    listingRate: Yup.string().required("Required"),
+    whitelist: Yup.string().required("Required"),
+    router: Yup.string().required("Required"),
+    liquidityPercent: Yup.string().required("Required"),
+    refundType: Yup.string().required("Required"),
+    liquidityLockTime: Yup.string().required("Required"),
   });
 
   const handleSubmit = (values, submitProps) => {
-
     console.log("Form values: ", values);
     submitProps.setSubmitting(false);
     submitProps.resetForm();
-    setPage(1)
+    setPage(1);
   };
 
   /////////////////
@@ -43,6 +60,7 @@ const LaunchpadForm = ({page, setPage}) => {
       {formik => (
         <Form className="p-4 bg-neutral-800 rounded-md flex flex-col gap-4">
           {page === 1 && <Step1 formik={formik} />}
+          {page === 2 && <Step2 formik={formik} />}
 
           {/* buttons  */}
           <div className="flex gap-3">
@@ -70,7 +88,178 @@ const LaunchpadForm = ({page, setPage}) => {
   );
 };
 
-const Step1 = ({formik}) => {
+const Step2 = ({ formik }) => {
+  const whitelistOptions = [
+    {
+      key: "Disabled",
+      value: "disabled",
+    },
+    {
+      key: "Enabled",
+      value: "enabled",
+    },
+  ];
+
+  const refundTypeOptions = [
+    {
+      key: "Refund",
+      value: "refund",
+    },
+    {
+      key: "Burn",
+      value: "burn",
+    },
+  ];
+
+  const routerOptions = [
+    {
+      key: "Select Router",
+      value: "",
+    },
+    {
+      key: "Pancakeswap",
+      value: "0x10ED43C718714eb63d5aA57B78B54704E256024E",
+    },
+    {
+      key: "ApeSwap",
+      value: "0xcF0feBd3f17CEf5b47b0cD257aCf6025c5BFf3b7",
+    },
+    {
+      key: "MDex",
+      value: "0xf400087A4c94c52C6540A325CB702DE3ee7CB37f",
+    },
+    {
+      key: "BiSwap",
+      value: "0x3a6d8cA21D1CF76F653A67577FA0D27453350dD8",
+    },
+    {
+      key: "KnightSwap",
+      value: "0x05E61E0cDcD2170a76F9568a110CEe3AFdD6c46f",
+    },
+  ];
+
+  return (
+    <>
+      {/* Input  */}
+      <FormikControl
+        control={"input"}
+        label={"Hardcap Tokens"}
+        type={"number"}
+        name={"hardcapTokens"}
+        placeholder={"Ex: 1000"}
+      />
+
+      {/* Input  */}
+      <FormikControl
+        control={"input"}
+        label={"Softcap Tokens"}
+        type={"number"}
+        name={"softcapTokens"}
+        placeholder={"Ex: 1000"}
+      />
+
+      {/* Input  */}
+      <FormikControl
+        control={"input"}
+        label={"Hardcap Tokens per user"}
+        type={"number"}
+        name={"hardcapTokensPerUser"}
+        placeholder={"Ex: 1000"}
+      />
+
+      {/* Radio  */}
+      <FormikControl
+        control={"radio"}
+        label={"Whitelist"}
+        name={"whitelist"}
+        options={whitelistOptions}
+      />
+
+      {/* sub and list rate */}
+      <div className="flex flex-col md:flex-row gap-3">
+        {/* Input  */}
+        <div className="flex-1">
+          <FormikControl
+            control={"input"}
+            label={"Subscription rate"}
+            type={"number"}
+            name={"subscriptionRate"}
+            placeholder={"Ex: 1000"}
+          />
+
+          <InputNote
+            note={`If I spend 1 BNB how many tokens will I receive?`}
+            styles={"mt-0"}
+          />
+        </div>
+
+        {/* Input  */}
+        <FormikControl
+          control={"input"}
+          label={"Listing rate"}
+          type={"number"}
+          name={"listingRate"}
+          placeholder={"Ex: 1000"}
+        />
+      </div>
+
+      {/* router and liquid */}
+      <div className="flex flex-col md:flex-row gap-3">
+        <div className="flex-1">
+          {/* router  */}
+          <FormikControl
+            control={"select"}
+            label={"Router"}
+            name={"router"}
+            options={routerOptions}
+          />
+        </div>
+
+        <div className="flex-1">
+          {/* Input  */}
+          <FormikControl
+            control={"input"}
+            label={"Liquidity Percent (%)"}
+            type={"number"}
+            name={"liquidityPercent"}
+            placeholder={"Ex: 70"}
+          />
+
+          <InputNote
+            note={`Enter the percentage of raised funds that should be allocated to Liquidity on (Min 51%, Max 100%)`}
+            styles={"mt-0"}
+          />
+        </div>
+      </div>
+
+      {/* refund and locktime */}
+      <div className="flex flex-col md:flex-row gap-3">
+        <div className="flex-1">
+          {/* refund  */}
+          <FormikControl
+            control={"select"}
+            label={"Refund"}
+            name={"refundType"}
+            options={refundTypeOptions}
+          />
+        </div>
+
+        <div className="flex-1">
+          {/* Input  */}
+          <FormikControl
+            control={"input"}
+            label={"Liquidity Lock Time (minutes)"}
+            type={"number"}
+            name={"liquidityLockTime"}
+            placeholder={"Ex: 20"}
+          />
+        </div>
+      </div>
+    </>
+  );
+};
+
+const Step1 = ({ formik }) => {
   const feeOptions = [
     {
       key: "5%",
--- components/shared/InputNote.jsx
+++ components/shared/InputNote.jsx
@@ -1,5 +1,5 @@
-const InputNote = ({ note }) => (
-  <div className="-mt-3">
+const InputNote = ({ note, styles }) => (
+  <div className={`-mt-3 ${styles}`}>
     <span className="text-blue-500 text-sm">{note}</span>
   </div>
 );
