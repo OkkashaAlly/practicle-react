a671931063565d87a142416f3928d5321825daa2
OkkashaAlly <steamnature006@gmail.com>
2022-10-31 20:52:27 +0300

    COMPONENTS: FormSteps - Dynamic formsteps component added

---
+++ components/shared/FormSteps.jsx
@@ -0,0 +1,70 @@
+import { RiCheckLine } from "react-icons/ri";
+
+const FormSteps = ({ step2 }) => {
+  const active = 2;
+
+  return (
+    <div className="my-4 hidden lg:flex gap-3">
+      <StepItem
+        title={"Verify Token"}
+        subtitle={"Enter the token address and verify"}
+        active={active}
+        number={1}
+      />
+      <StepItem
+        title={step2.title}
+        subtitle={step2.sub}
+        active={active}
+        number={2}
+      />
+      <StepItem
+        title={"Add Additional Info"}
+        subtitle={"Let people know who you are"}
+        active={active}
+        number={3}
+      />
+      <StepItem
+        title={"Finish"}
+        subtitle={"Review your information"}
+        active={active}
+        number={4}
+        removeLine
+      />
+    </div>
+  );
+};
+
+const StepItem = ({ title, subtitle, number, removeLine, active }) => (
+  <div className="flex flex-1">
+    <div className="flex gap-2">
+      <div className="">
+        {number < active ? (
+          <div className="bg-neutral-800 p-[0.2rem] border border-pink rounded-full">
+            <RiCheckLine className="h-6 w-6 text-pink " />
+          </div>
+        ) : (
+          <p
+            className={`h-6 w-6 p-4 flex items-center justify-center ${
+              active === number
+                ? "bg-rose-400"
+                : "bg-neutral-800 border border-neutral-900"
+            }   rounded-full`}
+          >
+            {number}
+          </p>
+        )}
+      </div>
+      <div className="">
+        <div className="flex-1 flex gap-3 items-center">
+          <h6>{title}</h6>{" "}
+          {!removeLine && (
+            <span className="h-[0.5px]  block bg-neutral-300 flex-1" />
+          )}
+        </div>
+        <p className="w-[80%] text-neutral-500">{subtitle}</p>
+      </div>
+    </div>
+  </div>
+);
+
+export default FormSteps;
--- pages/launchpad/create.jsx
+++ pages/launchpad/create.jsx
@@ -1,27 +1,16 @@
-import React from "react";
 import LaunchpadForm from "../../components/LaunchpadForm";
-
-const StepItem = ({ title, subtitle, number }) => (
-  <div className="flex w-full">
-    <div className="flex gap-2">
-      <p className="h-6 w-6 flex items-center justify-center  bg-rose-400 text-slate-800 rounded-full">
-        {number}
-      </p>
-      <div className="">
-        <div className="flex-1 flex gap-3 items-center">
-          <h6>{title}</h6>{" "}
-          <span className="h-[0.5px]  block bg-neutral-300 flex-1" />
-        </div>
-        <p className="w-[80%] text-neutral-500">{subtitle}</p>
-      </div>
-    </div>
-  </div>
-);
+import FormSteps from "../../components/shared/FormSteps";
 
 const create = () => {
+  const step2 = {
+    title: "DeFi Launchpad Info",
+    sub: "Enter the launchpad information that you want to raise , that should be enter all details about your presale",
+  };
+
   return (
     <div>
       {/* form  */}
+      <FormSteps step2={step2} />
       <LaunchpadForm />
     </div>
   );
