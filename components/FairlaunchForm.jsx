ebdd47b04853e6b120352d0c32fc9de532e944dd
OkkashaAlly <steamnature006@gmail.com>
2022-11-01 13:28:29 +0300

    COMPONENTS: FairLaunchForm - Dynamic Step2 intergrated

--- components/FairlaunchForm.jsx
+++ components/FairlaunchForm.jsx
@@ -23,11 +23,22 @@ const FairlaunchForm = ({ page, setPage }) => {
     tokenAddress: "",
     tokenCurrency: "bnb",
     feeOptions: "5%",
+    totalSellingAmount: "",
+    softcap: "",
+    router: "",
+    liquidity: "",
+    liquidityLockup: "",
   };
+
   const validationSchema = Yup.object({
     tokenAddress: Yup.string().required("Required"),
     tokenCurrency: Yup.string().required("Required"),
     feeOptions: Yup.string().required("Required"),
+    totalSellingAmount: Yup.string().required("Required"),
+    softcap: Yup.string().required("Required"),
+    router: Yup.string().required("Required"),
+    liquidity: Yup.string().required("Required"),
+    liquidityLockup: Yup.string().required("Required"),
   });
 
   const handleSubmit = (values, submitProps) => {
@@ -87,6 +98,90 @@ const FairlaunchForm = ({ page, setPage }) => {
         </Form>
       )}
     </Formik>
+  );
+};
+
+///////////////////////////
+// EXTENDED COMPONETS /////
+const Step2 = ({ formik }) => {
+  const routerOptions = [
+    {
+      key: "Select Router",
+      value: "",
+    },
+    {
+      key: "Pancakeswap",
+      value: "0x10ED43C718714eb63d5aA57B78B54704E256024E",
+    },
+    {
+      key: "ApeSwap",
+      value: "0xcF0feBd3f17CEf5b47b0cD257aCf6025c5BFf3b7",
+    },
+    {
+      key: "MDex",
+      value: "0xf400087A4c94c52C6540A325CB702DE3ee7CB37f",
+    },
+    {
+      key: "BiSwap",
+      value: "0x3a6d8cA21D1CF76F653A67577FA0D27453350dD8",
+    },
+    {
+      key: "KnightSwap",
+      value: "0x05E61E0cDcD2170a76F9568a110CEe3AFdD6c46f",
+    },
+  ];
+
+  return (
+    <>
+      {/* Input  */}
+      <FormikControl
+        control={"input"}
+        label={"Total selling amount"}
+        type={"text"}
+        name={"totalSellingAmount"}
+        placeholder={"Ex: 100"}
+      />
+
+      <FormikControl
+        control={"input"}
+        label={"Softcap"}
+        type={"text"}
+        name={"softcap"}
+        placeholder={"Ex: 100"}
+      />
+
+      {/* router  */}
+      <FormikControl
+        control={"select"}
+        label={"Router"}
+        name={"router"}
+        options={routerOptions}
+      />
+
+      <FormikControl
+        control={"input"}
+        label={"Liquidity (%)"}
+        type={"text"}
+        name={"liquidity"}
+        placeholder={"Ex: 50"}
+      />
+
+      <InputNote
+        note={`Enter the percentage of raised funds that should be allocated to Liquidity on (Min 51%, Max 100%)`}
+      />
+
+      <InputNote
+        note={`If I spend 1 BNB on how many tokens will I receive? Usually this amount is lower than presale rate to allow for a higher listing price on`}
+      />
+
+      <FormikControl
+        control={"input"}
+        label={"Liquidity lockup (minutes)"}
+        type={"text"}
+        name={"liquidityLockup"}
+        placeholder={"Ex: 5"}
+      />
+    </>
   );
 };
 
--- pages/launchpad/fairlaunch.jsx
+++ pages/launchpad/fairlaunch.jsx
@@ -2,23 +2,6 @@ import { useState } from "react";
 
 import { FairlaunchForm, FormSteps } from "../../components/";
 
-const StepItem = ({ title, subtitle, number }) => (
-  <div className="flex w-full">
-    <div className="flex gap-2">
-      <p className="h-6 w-6 flex items-center justify-center  bg-rose-400 text-slate-800 rounded-full">
-        {number}
-      </p>
-      <div className="">
-        <div className="flex-1 flex gap-3 items-center">
-          <h6>{title}</h6>{" "}
-          <span className="h-[0.5px]  block bg-neutral-300 flex-1" />
-        </div>
-        <p className="w-[80%] text-neutral-500">{subtitle}</p>
-      </div>
-    </div>
-  </div>
-);
-
 // ===============================
 // FAIR LAUNCH PAGE COMPONENT ====
 // ===============================
@@ -26,7 +9,7 @@ const fairlaunch = () => {
   const [page, setPage] = useState(1);
 
   const step2 = {
-    title: "DeFi Launchpad Info",
+    title: "DeFi Fairlaunch Info",
     sub: "Enter the launchpad information that you want to raise , that should be enter all details about your presale",
   };
 
