c20f5451657bd060b33b70cdd02ea3b31f9ab7e5
OkkashaAlly <steamnature006@gmail.com>
2022-11-04 22:35:37 +0300

    PAGES: index - Simple form

--- components/shared/InputNote.jsx
+++
@@ -1,7 +0,0 @@
-const InputNote = ({ note, styles }) => (
-  <div className={`-mt-3 ${styles} flex-1`}>
-    <span className="text-blue-500 text-sm">{note}</span>
-  </div>
-);
-
-export default InputNote;
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,43 @@
+import { useState } from "react";
 
 // =========================
 // HOME PAGE COMPONENT =====
 // =========================
-const Home = () => <>Hellow</>;
+const Home = () => {
+  const [inputValue, setInputValue] = useState("");
 
+  console.log(inputValue);
+
+  const handleSubmit = e => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className="flex px-4 justify-center h-screen w-screen items-center bg-neutral-700">
+      <form
+        onSubmit={handleSubmit}
+        className="flex flex-col gap-3 bg-white p-4 rounded-lg"
+      >
+        <h1 className="text-2xl font-semibold">QR Code Generator</h1>
+        <p className="text-neutral-600">
+          Please paste / enter a URL to create QR Code
+        </p>
+        <input
+          className="rounded p-2 border border-blue-500"
+          type="text"
+          placeholder="Enter a URL"
+          onChange={e => setInputValue(e.target.value)}
+        />
+        <button type="submit" className="p-3 rounded bg-blue-500 text-white">
+          Generate QR Code now
+        </button>
+
+        {/* QR Code */}
+        <div className=""></div>
+      </form>
+    </div>
+  );
+};
 // =======================
 // EXTENDED COMPONENTS ===
 
