d964ac08f01a6add2d38ab9d42e50c7048490f04
OkkashaAlly <steamnature006@gmail.com>
2022-11-05 09:45:02 +0300

    PAGES: index - Download QRCode png functionality added

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import { QRCode } from "react-qrcode-logo";
 
 // =========================
@@ -20,15 +20,31 @@ const Home = () => {
     setInputValue(e.target.value);
   };
 
+  const downloadCode = () => {
+    const canvas = document.getElementById("qrcode");
+
+    if (canvas) {
+      const pngUrl = canvas
+        .toDataURL("image/png")
+        .replace("image/png", "image/octet-stream");
+      let downloadLink = document.createElement("a");
+      downloadLink.href = pngUrl;
+      downloadLink.download = `qrcode.png`;
+      document.body.appendChild(downloadLink);
+      downloadLink.click();
+      document.body.removeChild(downloadLink);
+    }
+  };
+
   return (
-    <div className="flex px-4 justify-center h-screen w-screen items-center bg-neutral-700">
+    <div className="flex p-4 justify-center min-h-screen w-screen items-center bg-neutral-700">
       <form
         onSubmit={handleSubmit}
         className="flex flex-col gap-3 bg-white p-4 rounded-lg"
       >
         <h1 className="text-2xl font-semibold">QR Code Generator</h1>
         <p className="text-neutral-600">
-          Please paste / enter a URL to create QR Code
+          Please paste / enter a URL to create QR Code -- <span className="text-blackm font-semibold">and Download below</span>
         </p>
         <input
           className="rounded p-2 border border-blue-500"
@@ -44,12 +60,19 @@ const Home = () => {
         <div
           className={`${
             showQRCode && inputValue !== ""
-              ? "h-72 opacity-100"
-              : "h-0 opacity-0"
+              ? "h-fit opacity-100 flex"
+              : "h-0 opacity-0 hidden"
           } flex items-center duration-300 flex-col`}
         >
           <p>Scan Now</p>
-          <QRCode value={inputValue} size={250} />
+          <QRCode id="qrcode" value={inputValue} size={250} />
+          <button
+            className="p-3 bg-neutral-500 text-neutral-200 rounded mt-2"
+            type="button"
+            onClick={() => downloadCode()}
+          >
+            Download Code
+          </button>
         </div>
       </form>
     </div>
