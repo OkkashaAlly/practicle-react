faba6db573d528ce6ac7a69720f8d6877046c80e
OkkashaAlly <steamnature006@gmail.com>
2022-11-07 12:16:55 +0300

    PAGES: index - React Icons individual consumption

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,16 +1,21 @@
-import { useState, useRef } from "react";
-import { QRCode } from "react-qrcode-logo";
+import { RiHomeWifiLine } from "react-icons/ri";
 
 // =========================
 // HOME PAGE COMPONENT =====
 // =========================
 const Home = () => {
-  
-
-  
   return (
-    <div className="flex p-4 justify-center min-h-screen w-screen items-center bg-neutral-700">
-      Practicle React
+    <div className="flex flex-col p-4 justify-center min-h-screen w-screen items-center">
+      <h1 className="text-4xl font-bold">Practicle React</h1>
+
+      <div className="">
+        <p className="font-semibold">Icons</p>
+        <div className="flex">
+          {/* <RiHomeWifiLine color="#999" size="30px" /> */}
+          {/* <RiHomeWifiLine color="red" size="30rem" /> */}
+          <RiHomeWifiLine className="h-14 w-14 text-green-500" />
+        </div>
+      </div>
     </div>
   );
 };
