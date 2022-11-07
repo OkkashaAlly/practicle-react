3257fa49f214c10c4806e3ef04ce61a582786697
OkkashaAlly <steamnature006@gmail.com>
2022-11-07 12:22:13 +0300

    PAGES: index - React Icons context(multiple icons) consumption

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,22 +1,31 @@
-import { RiHomeWifiLine } from "react-icons/ri";
+import { IconContext } from "react-icons";
+import { RiHomeWifiLine, RiAtFill } from "react-icons/ri";
 
 // =========================
 // HOME PAGE COMPONENT =====
 // =========================
 const Home = () => {
-  return (
-    <div className="flex flex-col p-4 justify-center min-h-screen w-screen items-center">
-      <h1 className="text-4xl font-bold">Practicle React</h1>
+  const iconsStyle = {
+    color: "blue",
+    size: "5rem",
+  };
 
-      <div className="">
-        <p className="font-semibold">Icons</p>
-        <div className="flex">
-          {/* <RiHomeWifiLine color="#999" size="30px" /> */}
-          {/* <RiHomeWifiLine color="red" size="30rem" /> */}
-          <RiHomeWifiLine className="h-14 w-14 text-green-500" />
+  return (
+    <IconContext.Provider value={iconsStyle}>
+      <div className="flex flex-col p-4 gap-14 justify-center min-h-screen w-screen items-center">
+        <h1 className="text-4xl font-bold">Practicle React</h1>
+
+        <div className="">
+          <p className="font-semibold text-center mb-6">Icons</p>
+          <div className="flex">
+            {/* <RiHomeWifiLine color="#999" size="30px" /> */}
+            {/* <RiHomeWifiLine color="red" size="30rem" /> */}
+            <RiHomeWifiLine color="green" />
+            <RiAtFill />
+          </div>
         </div>
       </div>
-    </div>
+    </IconContext.Provider>
   );
 };
 // =======================
