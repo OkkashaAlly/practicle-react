9ab4acb231915f6b66db422053602ea67178ebc7
OkkashaAlly <steamnature006@gmail.com>
2022-11-10 08:55:46 +0300

    PAGES: index - Simple video player

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,13 +1,11 @@
-import { useState } from "react";
-import Datepicker from "react-datepicker";
-import "react-datepicker/dist/react-datepicker.css";
+// import ReactPlayer from "react-player/youtube";
+import dynamic from "next/dynamic";
+const ReactPlayer = dynamic(() => import("react-player"), { ssr: false });
 
 // =========================
 // HOME PAGE COMPONENT =====
 // =========================
 const Home = () => {
-  const [selectedDate, setSelectedDate] = useState(new Date());
-
   return (
     <div
       id="root"
@@ -16,21 +14,9 @@ const Home = () => {
       <h1 className="text-4xl font-bold">Practicle React</h1>
 
       <div className="text-center">
-        <p>React Datepicker - </p>
-        {/* {console.log(selectedDate?.getDay() + 3)} */}
+        <p>React Video player </p>
         <div className="">
-          <Datepicker
-            selected={selectedDate}
-            onChange={date => setSelectedDate(date)}
-            className="p-3 border border-green-500"
-            dateFormat="dd/MM/yyyy"
-            minDate={new Date()}
-            // maxDate={new Date()}
-            filterDate={date => date.getDay() !== 0 && date.getDay() !== 6} //filter weekends
-            isClearable
-            // showYearDropdown //deprecated
-            // scrollableMonthYearDropdown
-          />
+          <ReactPlayer url="https://www.youtube.com/watch?v=ysz5S6PUM-U" />
         </div>
       </div>
     </div>
@@ -38,17 +24,6 @@ const Home = () => {
 };
 // =======================
 // EXTENDED COMPONENTS ===
-const Input = ({ type, name, placeholder, value, action, focus }) => (
-  <input
-    type={type}
-    name={name}
-    placeholder={placeholder}
-    value={value}
-    onChange={action}
-    onFocus={focus}
-    className="p-3 border border-green-500"
-  />
-);
 
 // EXPORT =========
 export default Home;
